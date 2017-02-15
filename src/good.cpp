df03ea83d8288f8450d50ec2a5e27a39819be25b
pershik <k.pershik@gmail.com>
2017-02-16 02:30:16 +0300

    test

---
+++ src/good.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int c;
+
+class a {
+    int b()
+    {
+            return 25;
+    }
+};
+
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
+}
